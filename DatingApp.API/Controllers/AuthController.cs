29aac5f9281f2a36b535317a502183d0abe55193
Victor Ramirez <VRAMIREZ@gocontec.com>
2019-10-15 22:09:42 -0500

    End of section 5

--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -46,7 +46,8 @@ namespace DatingApp.API.Controllers
 
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
         {
-            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(),
+            userForLoginDto.Password);
 
             if (userFromRepo == null)
                 return Unauthorized();
